42eda78b1c721438dd9f0945188754101418be79
AndreiKandratsenka <andrei_kandratsenka@epam.com>
2019-08-21 15:46:51 +0300

    changed webpack config for extracting styles

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,7 +38,15 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
           {
             loader: "css-loader",
             options : {
